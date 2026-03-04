4cefa8f6e71f2e930752a9c0ff2087ff2f078283
Luke <44572009+lawrelu@users.noreply.github.com>
2026-03-04 15:36:12 +0000

    Create Models/Organisation.cs file

--- Models/Organisation.cs
+++ Models/Organisation.cs
@@ -1,16 +1,15 @@
 using System;
 using System.Collections.Generic;
-
-namespace Stagedoor.Models {
+namespace Models {
     public class Organisation {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public string CloudProvider { get; set; } // "OneDrive" or "GoogleDrive"
-        public string CloudAccessToken { get; set; }
-        public string CloudRefreshToken { get; set; }
-        public DateTime? LastCloudSync { get; set; }
+        public string Address { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public ICollection<Show> Shows { get; set; } = new List<Show>();
     }
 }
