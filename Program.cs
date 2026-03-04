8131a7c0482a5e57f99d7d69441f95790ef7d194
Luke <44572009+lawrelu@users.noreply.github.com>
2026-03-04 15:46:03 +0000

    Create Program.cs file with server setup code

--- Program.cs
+++ Program.cs
@@ -1,20 +1,27 @@
 using Microsoft.EntityFrameworkCore;
-using Data;
 using Services;
+using Data;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplicationBuilder.CreateBuilder(args);
+
+// Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure DbContext
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<StagedoorDbContext>(options => options.UseSqlServer(connectionString));
+
+// Register Services
 builder.Services.AddScoped<IOrganisationService, OrganisationService>();
 builder.Services.AddScoped<IShowService, ShowService>();
 builder.Services.AddScoped<IPerformanceService, PerformanceService>();
 builder.Services.AddScoped<IMemberService, MemberService>();
 builder.Services.AddScoped<IAttendanceService, AttendanceService>();
 builder.Services.AddScoped<INFCReaderService, NFCReaderService>();
+
+// Configure CORS
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowAll", builder => {
         builder.AllowAnyOrigin()
@@ -25,13 +32,14 @@ builder.Services.AddCors(options => {
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseAuthorization();
 app.UseCors("AllowAll");
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
